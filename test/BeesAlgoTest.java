0b626321656d7f3ec2f5fb1a1e51eb02a74a9c09
Janusz <jmirosla@student.agh.edu.pl>
2015-04-21 09:05:19 +0200

    initial commit

---
+++ test/BeesAlgoTest.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Janusz on 21-04-2015.
+ */
+public class BeesAlgoTest {
+    public static void main(String[] args) {
+        BeesAlgo beesAlgo = new BeesAlgo();
+        beesAlgo.run();
+        double[] optimalPoint=beesAlgo.optimalPoint();
+        System.out.print("Point (");
+        for (int j = 0; j < beesAlgo.var; j++) {
+            System.out.print(optimalPoint[j]);
+            if(j!=beesAlgo.var-1){
+                System.out.print(",");
+            }
+        }
+        System.out.println(")");
+        System.out.println("Value " + beesAlgo.optimalValue());
+    }
+}
