427972eaeafad626bff5324b063cb21b8de5d39c
Janusz <jmirosla@student.agh.edu.pl>
2015-05-14 21:57:41 +0200

    add checking time

--- test/BeesAlgoTest.java
+++ test/BeesAlgoTest.java
@@ -6,7 +6,9 @@ import java.io.FileNotFoundException;
 
 public class BeesAlgoTest {
     public static void main(String[] args) throws FileNotFoundException {
-        BeesAlgo beesAlgo = new BeesAlgo(0.2, 100, 10, 4, 4, 200, 8, "C:\\Users\\Janusz\\IdeaProjects\\BeesTSP\\TSPLIB\\berlin52.tsp");
+        long start = System.currentTimeMillis();
+
+        BeesAlgo beesAlgo = new BeesAlgo(0.2, 100, 10, 4, 4, 2000000, 8, "C:\\Users\\Janusz\\IdeaProjects\\BeesTSP\\TSPLIB\\berlin52.tsp");
 
         beesAlgo.startAlgo();
         beesAlgo.setDistances();
@@ -14,5 +16,7 @@ public class BeesAlgoTest {
         beesAlgo.init();
         beesAlgo.fullRandom();
         beesAlgo.run();
+        long end = System.currentTimeMillis();
+        System.out.println("Time: " + (double)(end-start)/1000);
     }
 }
