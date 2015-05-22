db5b3995d4afa48dda44b6f5925dc164bb9fb763
Janusz <jmirosla@student.agh.edu.pl>
2015-05-22 22:07:15 +0200

    remove unused imports

--- src/BeesGUI.java
+++ src/BeesGUI.java
@@ -6,8 +6,6 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
